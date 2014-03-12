ddc95d96859654c90db82a7e708fea68eb29c183
Ben Shope <ben@benshope.com>
2014-03-12 05:40:58 -0700

    Edit Contact Form

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from google.appengine.api import mail
 
 class SendMail(webapp2.RequestHandler):
     def post(self):
-        args = [self.request.get(x) for x in ['name', 'email', 'message']]
+        data = [self.request.get(x) for x in ['name', 'email', 'message']]
         mail.send_mail(sender='nimajnebs@gmail.com', 
             to='nimajnebs@gmail.com', 
-            subject='Contact Form Message', 
-            body='From: {0} | {1} \nMessage: {2}'.format(*args))
+            subject='Contact From '.format(data[0]), 
+            body='From: {} | {} \nMessage: {}'.format(*data))
         self.response.out.write('Thanks!  Your message has been sent.')
 
 app = webapp2.WSGIApplication([
