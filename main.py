a3e37f7fb6aac1a55e59f027315273a32fb20da7
Ben Shope <ben@benshope.com>
2014-03-12 05:31:07 -0700

    Improve Contact Form

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from google.appengine.api import mail
 
 class SendMail(webapp2.RequestHandler):
     def post(self):
-        mail.send_mail(sender="Ben Shope <nimajnebs@gmail.com>", to="Ben Shope <nimajnebs@gmail.com>", subject="BenShope Contact Form Message", body="From: {0} | {1} \nMessage: {2}".format(self.request.get('name'), self.request.get('email'), self.request.get('message')))
+        args = [self.request.get(x) for x in ['name', 'email', 'message']]
+        mail.send_mail(sender='nimajnebs@gmail.com', 
+            to='nimajnebs@gmail.com', 
+            subject='Contact Form Message', 
+            body='From: {0} | {1} \nMessage: {2}'.format(*args))
         self.response.out.write('Thanks!  Your message has been sent.')
 
 app = webapp2.WSGIApplication([
