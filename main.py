b6324c0a885eb6c4d50a4f6d8df8b5be67efc222
nargroves <nargroves@gmail.com>
2014-03-29 19:50:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from google.appengine.api import mail
 class SendMail(webapp2.RequestHandler):
     def post(self):
         data = [self.request.get(x) for x in ['name', 'email', 'message']]
-        mail.send_mail(sender='nimajnebs@gmail.com', 
-            to='nimajnebs@gmail.com', 
+        mail.send_mail(sender='nate@argrov.es', 
+            to='nate@argrov.es', 
             subject='Contact From '.format(data[0]), 
             body='From: {} | {} \nMessage: {}'.format(*data))
         self.response.out.write('Thanks!  Your message has been sent.')
